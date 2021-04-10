fdf93b5cd0ae7b013758df3af9ec31f5496f0f69
CoolSheff131 <a.nikulsheev@gmail.com>
2021-04-10 17:12:49 +0800

    аутентификация через google (без базы данных)

--- app/src/main/java/com/example/lostandfound/HomeActivity.java
+++ app/src/main/java/com/example/lostandfound/HomeActivity.java
@@ -1,10 +1,6 @@
 package com.example.lostandfound;
 
-import java.util.Scanner;
-
 public class HomeActivity {
 
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
+
 }
--- app/src/main/java/com/example/lostandfound/MainActivity.java
+++ app/src/main/java/com/example/lostandfound/MainActivity.java
@@ -1,15 +1,72 @@
 package com.example.lostandfound;
 
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button logBtn;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {//TODO авторизация через Гугл
+        //TODO подключить базу
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Hello");//Мой коммит
+        logBtn = findViewById(R.id.button2);
+
+        logBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestEmail()
+                        .build();
+                GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(MainActivity.this, gso);
+
+
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, 1);
+                //GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this); Проверка что уже авторизированаы
+            }
+        });
+
+
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == 1) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            Toast.makeText(MainActivity.this, account.getEmail(),Toast.LENGTH_LONG).show();
+
+            // Signed in successfully, show authenticated UI.
+            Toast.makeText(MainActivity.this,"Signed",Toast.LENGTH_LONG).show();
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w( "TAG", "signInResult:failed code=" + e.getStatusCode());
+            Toast.makeText(MainActivity.this,"Failed "+e,Toast.LENGTH_LONG).show();
+        }
     }
 }
