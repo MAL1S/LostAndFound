567f0ea04ddfff16065e6acc63f6bd558fa15a26
gera275 <kolosov.slava.70@gmail.com>
2021-04-10 13:44:58 +0800

    созданы классы AddActivity , LostActivity , FoundActivity , SettingActivity

---
+++ app/src/main/java/com/example/lostandfound/FoundActivity.java
@@ -0,0 +1,17 @@
+package com.example.lostandfound;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Scanner;
+
+public class FoundActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
