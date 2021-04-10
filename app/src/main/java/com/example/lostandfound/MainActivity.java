5af1b1987eae383e900c63a282627f7b27e6e29e
CoolSheff131 <a.nikulsheev@gmail.com>
2021-04-10 20:04:27 +0800

    аутентификация через google с базой данных!

--- app/src/main/java/com/example/lostandfound/MainActivity.java
+++ app/src/main/java/com/example/lostandfound/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -15,13 +16,22 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
     private Button logBtn;
+    private FirebaseAuth mAuth;
+    GoogleSignInClient mGoogleSignInClient;
+    final int RC_SIGN_IN=2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {//TODO авторизация через Гугл
-        //TODO подключить базу
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         logBtn = findViewById(R.id.button2);
@@ -30,43 +40,64 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestIdToken("13014954881-6gft53rgeru7qfidf6uhtu9r29ce59s0.apps.googleusercontent.com")
                         .requestEmail()
                         .build();
-                GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(MainActivity.this, gso);
+
+
+                mGoogleSignInClient = GoogleSignIn.getClient(MainActivity.this, gso);
 
 
                 Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-                startActivityForResult(signInIntent, 1);
+                startActivityForResult(signInIntent, RC_SIGN_IN);
                 //GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this); Проверка что уже авторизированаы
             }
         });
 
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
 
+
+        //FirebaseAuth.getInstance().signOut(); Выход из аккаунта
     }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-        if (requestCode == 1) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
+        if (requestCode == RC_SIGN_IN) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             handleSignInResult(task);
         }
     }
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-            Toast.makeText(MainActivity.this, account.getEmail(),Toast.LENGTH_LONG).show();
-
-            // Signed in successfully, show authenticated UI.
-            Toast.makeText(MainActivity.this,"Signed",Toast.LENGTH_LONG).show();
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);//Ошибка
+            firebaseAuthWithGoogle(account.getIdToken());
         } catch (ApiException e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
             Log.w( "TAG", "signInResult:failed code=" + e.getStatusCode());
-            Toast.makeText(MainActivity.this,"Failed "+e,Toast.LENGTH_LONG).show();
+
         }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(MainActivity.this,"Signed",Toast.LENGTH_LONG).show();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithCredential:failure", task.getException());
+                            Toast.makeText(MainActivity.this,"Failed ",Toast.LENGTH_LONG).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
     }
 }
