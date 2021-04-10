93ec90d016c3c4222e3710e4276284bd2ed9f0bd
gera275 <kolosov.slava.70@gmail.com>
2021-04-10 12:39:54 +0800

    Create HomeActivity

--- app/src/main/java/com/example/lostandfound/HomeActivity.java
+++ app/src/main/java/com/example/lostandfound/HomeActivity.java
@@ -1,5 +1,10 @@
 package com.example.lostandfound;
 
+import java.util.Scanner;
+
 public class HomeActivity {
 
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
 }
