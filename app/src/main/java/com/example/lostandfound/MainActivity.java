c0dc1ef05739e7dd0a37df7ec392d9742b75a4db
luxorylife <lukash.201@mail.ru>
2021-04-10 18:02:49 +0800

    first test

--- app/src/main/java/com/example/lostandfound/MainActivity.java
+++ app/src/main/java/com/example/lostandfound/MainActivity.java
@@ -4,7 +4,32 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
@@ -16,6 +41,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.FileNotFoundException;
@@ -23,9 +49,27 @@ import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
+    private TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.tv);
+
+        Intent i1 = getIntent();
+        if (i1 != null)
+            tv.setText(i1.getStringExtra("lon") + " " + i1.getStringExtra("lat"));
+
+        button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, MapsActivity2.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/lostandfound/MapsActivity2.java
@@ -0,0 +1,148 @@
+package com.example.lostandfound;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallback {
+
+    private LocationManager locationManager;
+    private Location location;
+    private Marker marker;
+
+    FloatingActionButton button;
+
+    private GoogleMap mMap;
+
+    private boolean granted = false;
+    private int LOCATION_PERMISSION = 1001;
+    private LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            if (location == null){
+                return;
+            }else{
+                showLocation(marker.getPosition());
+            }
+        }
+    };
+
+    private void showLocation(LatLng ll){
+        System.out.println(ll.latitude);
+        System.out.println(ll.longitude);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps2);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map2);
+        mapFragment.getMapAsync(this);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        button = findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MapsActivity2.this, MainActivity.class);
+                i.putExtra("lat", String.valueOf(marker.getPosition().latitude));
+                i.putExtra("lon", String.valueOf(marker.getPosition().longitude));
+                startActivity(i);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (granted || checkPermission()) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000 * 10, 20, locationListener);
+            if (locationManager != null){
+                location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if (marker != null)
+                    showLocation(marker.getPosition());
+            }
+        }
+    }
+
+    private boolean checkPermission(){
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION);
+            return false;
+        } else{
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == LOCATION_PERMISSION){
+            granted = true;
+            for (int i = 0; i < grantResults.length; i++){
+                if (grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                    granted = false;
+                }
+            }
+        }else{
+            granted = false;
+        }
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                marker.remove();
+                marker = mMap.addMarker(new MarkerOptions().position(latLng).title("Marker").draggable(true));
+            }
+        });
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(location.getLatitude(), location.getLongitude());
+
+        marker = mMap.addMarker(new MarkerOptions().position(sydney).title("Marker").draggable(true));
+
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition.Builder()
+                .target(sydney)
+                .zoom(14)
+                .build()));
+
+
+    }
+}
