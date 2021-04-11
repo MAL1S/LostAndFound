ca6399cb79dfb03544a3163802b93b7335a479a9
luxorylife <lukash.201@mail.ru>
2021-04-11 11:19:38 +0800

    Вычисление ближайших объявлении к пользователю

--- app/src/main/java/com/example/lostandfound/FoundActivity.java
+++ app/src/main/java/com/example/lostandfound/FoundActivity.java
@@ -1,6 +1,11 @@
 package com.example.lostandfound;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.Network;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -8,9 +13,11 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,6 +26,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,8 +38,9 @@ public class FoundActivity extends AppCompatActivity {
     TextView result; // получаем из firebase
     DatabaseReference dbRef; // firebase object
     HashMap<String, Record> list;
-
-
+    private LocationManager locationManager;
+    private Location location;
+    ArrayList<Record> closest; //>лижайшие к пользователю объявления
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,17 +51,16 @@ public class FoundActivity extends AppCompatActivity {
         cardsList = findViewById(R.id.rv_found_cards);
 
 
-
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         cardsList.setLayoutManager(layoutManager);
 
         cardsList.setHasFixedSize(true);
 
-        cardsAdapter = new CardsAdapter(30 , this , "Собака" , "Найдена собака");
+        cardsAdapter = new CardsAdapter(30, this, "Собака", "Найдена собака");
         cardsList.setAdapter(cardsAdapter);
 
 
-        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.navigation);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
 
         Menu menu = bottomNavigationView.getMenu();
         MenuItem menuItem = menu.getItem(1);
@@ -60,21 +69,21 @@ public class FoundActivity extends AppCompatActivity {
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
+                switch (item.getItemId()) {
 
 
                     case R.id.account:
-                        Intent intent = new Intent(FoundActivity.this , AccountActivity.class);
+                        Intent intent = new Intent(FoundActivity.this, AccountActivity.class);
                         startActivity(intent);
                         break;
 
 
                     case R.id.lost:
-                        Intent intent1 = new Intent(FoundActivity.this , LostActivity.class);
+                        Intent intent1 = new Intent(FoundActivity.this, LostActivity.class);
                         startActivity(intent1);
                         break;
                     case R.id.add:
-                        Intent intent2 = new Intent(FoundActivity.this , AddActivity.class);
+                        Intent intent2 = new Intent(FoundActivity.this, AddActivity.class);
                         startActivity(intent2);
                         break;
                 }
@@ -88,11 +97,19 @@ public class FoundActivity extends AppCompatActivity {
         dbRef.child("0").child("record").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                GenericTypeIndicator<HashMap<String,Record>> r = new GenericTypeIndicator<HashMap<String,Record>>() {};
+                GenericTypeIndicator<HashMap<String, Record>> r = new GenericTypeIndicator<HashMap<String, Record>>() {
+                };
                 list = snapshot.getValue(r);
+                closest = new ArrayList<>();
                 for (Map.Entry<String, Record> entry : list.entrySet()) {
                     System.out.println(entry.getKey() + " " + entry.getValue());
+                    LatLng coor = new LatLng(entry.getValue().lat,entry.getValue().log);
+                    LatLng me = new LatLng(location.getLatitude(),location.getLongitude());
+                    if(Math.abs(coor.latitude - me.latitude)<=0.01 &&Math.abs(coor.longitude - me.longitude)<=0.01){
+                        closest.add(entry.getValue());//Добавляем ближайшие к пользователю
+                    }
                 }
+
             }
 
             @Override
@@ -103,4 +120,5 @@ public class FoundActivity extends AppCompatActivity {
 
 
     }
+
 }
